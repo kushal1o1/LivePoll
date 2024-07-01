7f8d467d2d27514d4e24babcae86fc63e74189c2
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2024-07-01 18:01:51 +0545

    buid:viewset

--- BackEnd/polls/views.py
+++ BackEnd/polls/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Poll, Choice
+from .serializers import PollSerializer, ChoiceSerializer
+
+class PollViewSet(viewsets.ModelViewSet):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+class ChoiceViewSet(viewsets.ModelViewSet):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
