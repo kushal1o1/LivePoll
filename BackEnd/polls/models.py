42dbfb4bec88a7947f191dc01fa158434e2e7453
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2024-07-01 17:48:42 +0545

    build:model for poll and choices

--- BackEnd/polls/models.py
+++ BackEnd/polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, related_name='choices', on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
